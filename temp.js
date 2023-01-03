26edc61b5580b15ccda50aa2e056b8828a254534
Neerajkumari99 <95603035+Neerajkumari99@users.noreply.github.com>
2023-01-03 23:23:35 +0530

    nameage

---
+++ temp.js
@@ -0,0 +1,13 @@
+let user_name = "neeraj";
+let password = "fourty";
+
+//for login
+
+let username = "neeraj";
+let passWord = "fourty";
+
+if ((user_name == username) && (password == passWord)) {
+  console.log("user can login");
+} else {
+  console.log("you can't login");
+}
